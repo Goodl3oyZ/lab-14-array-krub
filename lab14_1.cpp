55f0bcb3ae8113ac4d8e3d32b3049e416f8d38c6
Goodl3oyZ <panudetzx2@gmail.com>
2023-01-13 16:35:04 +0700

    14.1 sohard

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,47 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[])
+{
+    for(int i=0;i<N;i++)
+    {
+        B[0]+=A[i];
+        if(i==N-1)
+            B[0]=B[0]/N;
+    }
+    for(int j=0;j<N;j++)
+    {
+        B[1]+=pow((A[j]-B[0]),2);
+        if(j==N-1)
+            B[1]=sqrt(B[1]/(N));
+    }
+    B[2]=1;
+    for(int k=0;k<N;k++)
+    {
+        B[2]*=A[k];
+        if(k==N-1)
+        {
+            B[2]=pow(B[2],(1.0/N));
+        }
+    }
+    for(int l=0;l<N;l++)
+    {
+        B[3]+=1/A[l];
+        if(l==N-1)
+            B[3]=N/(B[3]);
+    }
+    for(int m=0;m<N;m++)
+    {
+        if(A[m]>B[4])
+            B[4]=A[m];
+        else B[4]=B[4];
+    }
+    B[5]=2;
+    for(int n=0;n<N;n++)
+    {
+        if(A[n]<B[5])
+            B[5]=A[n];
+        else B[5]=B[5];
+    }
+}
