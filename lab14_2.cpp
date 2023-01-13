9de239ab37510e273904efa8524f27eb20384057
Goodl3oyZ <panudetzx2@gmail.com>
2023-01-13 17:15:00 +0700

    14.2

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,27 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void updateImage(bool image[N][M],int s,int x,int y)
+{
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            if(sqrt(pow((i-x),2)+pow((j-y),2))<=s-1)
+                image[i][j]=1;
+        }
+    }
+}
+void showImage(const bool image[N][M])
+{
+    cout<<"------------------------------------------------------------------------"<<endl;
+    for(int i=0;i<N;i++){
+        cout<<"|";
+        for(int j=0;j<M;j++){
+            if(image[i][j]==1)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+        cout<<"|"<<endl;
+    }
+    cout<<"------------------------------------------------------------------------"<<endl;
+}
